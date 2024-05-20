1b0acbecbbff9d3e419c03d4f4160fe16f7a4ceb
adamggrim <97310482+adamggrim@users.noreply.github.com>
2024-05-20 01:09:56 -0400

    Rephrased comments

--- forecastcmd/constants.py
+++ forecastcmd/constants.py
@@ -1,10 +1,10 @@
-# String printed when asking the user for another zip code
+# String printed to prompt the user for another zip code
 ANY_OTHER_ZIP_STR = 'Any other zip code? (5 digits):'
 
 # Set of strings for selecting Celsius
 CELSIUS_STRS = {'celsius', 'c'}
 
-# String printed when asking the user to enter a temperature scale
+# String printed to prompt the user to enter a temperature scale
 ENTER_TEMP_SCALE_STR = 'Enter a temperature scale (Celsius [C] or Fahrenheit [F]):'
 
 # String printed when the previous temperature scale input was invalid
@@ -13,7 +13,7 @@ ENTER_VALID_TEMP_SCALE_STR = 'Please enter Celsius (C) or Fahrenheit (F):'
 # String printed when the previous zip code input was invalid
 ENTER_VALID_ZIP_STR = 'Please enter a valid zip code:'
 
-# String printed when asking the user to enter a zip code
+# String printed to prompt the user to enter a zip code
 ENTER_ZIP_STR = 'Enter zip code (5 digits):'
 
 # String printed when the user exits the program
