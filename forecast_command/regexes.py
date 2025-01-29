3b36ad02035233b235223fcecd7533ca3dd42cd7
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-01-28 23:58:19 -0500

    Update regexes

--- forecast_command/regexes.py
+++ forecast_command/regexes.py
@@ -4,35 +4,39 @@ import re
 class ParsingRegexes:
     """
     Compiled regular expressions and string for parsing forecast data.
-    
+
     Attributes:
-        NOT_TEMPS_LOOKAHEAD: Regular expression lookahead pattern that 
+        _WINDS_RANGE: Regular expression pattern that captures the 
+            substring following the first number in a wind speed range.
+
+        _NOT_TEMPS_LOOKAHEAD: Regular expression lookahead pattern that 
             ignores numbers in the forecast that are not temperatures.
 
-        AM_PM_SPACE: Compiled regular expression object that captures 
-            the boundary between a time value and a.m. or p.m.
+        AM_PM_BOUNDARY: Compiled regular expression object that 
+            captures the boundary between a time value and a.m. or p.m.
         AM_PM_FORMAT: Compiled regular expression object that captures 
             a.m. and p.m. strings for reformatting.
-        SPACES: Compiled regular expression object that captures 
-            duplicate spaces and trailing whitespace.
+        DUPLICATE_SPACES: Compiled regular expression object that 
+            captures duplicate spaces and trailing whitespace.
         TEMPS_FINDER: Compiled regular expression object that captures 
             numbers in a forecast that are temperatures.
         WINDS_FINDER: Regular expression pattern that captures numbers 
             in the forecast that are wind speeds.
     """
-    NOT_TEMPS_LOOKAHEAD: str = (
-        r'(?!\spercent|%|\sa\.m\.|\sp\.m\.|\sto|\smph|\sand|\sof\san'
+    _NOT_TEMPS_LOOKAHEAD: str = (
+        r'(?!\.\d|\spercent|%|\sa\.m\.|\sp\.m\.|\sto|\smph|\sand|\sof\san'
         r'|\sinch)'
     )
 
-    AM_PM_SPACE: re.Pattern[str] = re.compile(r'(?<=\d)(?=am|pm)')
+    AM_PM_BOUNDARY: re.Pattern[str] = re.compile(r'(?<=\d)(?=am|pm)')
     AM_PM_FORMAT: re.Pattern[str] = re.compile(r'(?<=\d\s(a|p))m\.?')
-    SPACES: re.Pattern[str] = re.compile(r'(?<=\s)\s|\s+$')
+    DUPLICATE_SPACES: re.Pattern[str] = re.compile(r'(?<=\s)\s|\s+$')
+    MPH_FINDER: re.Pattern[str] = re.compile(r'\bmph\b')
     TEMPS_FINDER: re.Pattern[str] = re.compile(
-        r'\b(-?\d{1,3})\b' + NOT_TEMPS_LOOKAHEAD
+        r'\b(-?\d{1,3})\b' + _NOT_TEMPS_LOOKAHEAD
     )
     WINDS_FINDER: re.Pattern[str] = re.compile(
-        r'\b(\d{1,3})(?:\s+to\s+(\d{1,3}))?\smph'
+        r'\b(\d{1,3})(?=\s(mph|to\s\d{1,3}\smph))'
     )
 
 
